68eb781bf0b6b3d904c7da7d5c6bbf354af1d2e1
BBSpace Builder <bbspace-builder@users.noreply.github.com>
2026-09-16 21:24:12 +0800

    fix: prefer official Maven repos for GitHub Actions plugin resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,6 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        // 国内镜像优先，网络受限时也能解析插件
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +10,10 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        // 国内镜像作为兜底
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
     }
 }
 plugins {
@@ -30,8 +30,6 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
         maven("https://jitpack.io") {
@@ -39,6 +37,8 @@ dependencyResolutionManagement {
                 includeGroup("com.github.naaammme")
             }
         }
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
     }
 }
 
